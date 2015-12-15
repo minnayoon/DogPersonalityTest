25db420e9eeb8062694563c5c5f0c1672c1b3f92
Minna Yoon <minnayoon@Minnas-MacBook-Pro.local>
2015-12-14 18:41:35 -0800

    William help

---
+++ PupMatch.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+$("#ESTJ").hide();
+
+//       var questionOne = $("q1")
+//       var questionTwo = $("q2")
+//       var questionThree = $("q3")
+//       var questionFour = $("q4")
+
+
+
+// };
+// $(#results).click(questionOne.value)
+//       console.log("print E")
+});
+//       questionTwo = 
+//             if (q2 === "S") {};
+
+//             else (q2 === "N")
+//       };
+//       else (q1 === "E"){
+
+//       };
+// });
+
+
+// ShowResults = function()
+
+// var resultsOptionOne = $(.INFP). 
+
+// (.Submit).onclick(ShowResults(#results))
+
+//  if(ShowResults){
+//       q1 = answer(I)
+//       q2 = answer(N)
+//       q3 = answer()
+//       q4 = answer()
+//  }
+//  ("#results").text())
